9363dbe9c7e228e04c109536ed48ec9ee9ba8942
Joe Mankin <joseaphmankin@gmail.com>
2018-08-11 21:34:49 -0500

    Word Selector and dashes pushed to div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,57 @@
 // Player Instructions on how to begin. Wins start at zero. Guesses start at 8
-let Wins = 0;
-let Guesses = 8;
+let wins = 0;
+let guesses = 8;
 
-// Computer picks a random puzzle & splits them apart
+// Computer picks a random word, puts that word in an array. & splits them apart
 
-let puzzles =["Dracula", "Mummy", "Wolfman", "Frankenstien",];
-let randomPuzzle = puzzles[Math.floor(Math.random() * puzzles.length)];
+let word =["Dracula", "Mummy", "Wolfman", "Frankenstein",];
+let randChoice = word[Math.floor(Math.random() * word.length)];
 
-    console.log(randomPuzzle); 
+    console.log(randChoice); 
 
-let splitPuzzle = randomPuzzle.split(""); 
+let wordLength = randChoice.length;
+console.log(wordLength);
 
-    console.log(splitPuzzle);
+let display = [wordLength];
+console.log=(display);
 
-//Player presses Start button and puzzle loads
+let win = wordLength;
+let letters = randChoice.split('')
 
-function myFunction() {
-    document.getElementById("puzzleDiv").innerHTML = splitPuzzle;
+let attemptsLeft = 8
+let output="";
+let userLetter="";
+
+let setup = function(){
+    for (let i=0; i < randChoice.length; i++){
+        display[i] = "_ ";
+        output = output + display[i];
+    }
+    document.getElementById("puzzleDiv").innerHTML = output; 
+    output ="";
+}
+
+window.onload = function(){
+    setup();
+}
+
+
+//Player presses Start button and puzzle loads and scoreboards are set
+
+function start() {
+
+    document.getElementById("lives").innerHTML = guesses;
+  
+    document.getElementById("wins").innerHTML = wins;
   }
 
 // Player chooses a letter (from keyboard, but possibly from clicking buttons)
 
 document.onkeyup = function() {
     let userguess = String.fromCharCode(event.keyCode).toUpperCase();
-
-    console.log(userguess);
 }
+
+
 
 // Player selection either causes puzzle letters to reveal, or causes guesses to increment down
 
