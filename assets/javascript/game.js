836a1c7804e1520ee87d84a31cf56426fd5a8884
Joe Mankin <joseaphmankin@gmail.com>
2018-08-16 22:38:27 -0500

    Cleaned up JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,14 @@ let haunting_audio = document.getElementById('haunting');
 
 
 // Index of puzzles, with the randomizer picking one. 
-let puzzles = ["DRACULA", "THE MUMMY", "THE WOLFMAN", "FRANKENSTEIN", "THE BLOB", "THE BRIDE OF FRANKENSTEIN", "THE CREATURE FROM THE BLACK LAGOON"];
+let puzzles = ["DRACULA", "MUMMY", "WOLFMAN", "FRANKENSTEIN"];
 let word = puzzles[Math.floor(Math.random() * puzzles.length)];
 
 // An Array to hold the puzzle blanks/letters and one to hold all the available guesses
 let secret = [];
 let available = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "];
 let pickedLtrs = [];
+// let wordDisplay = "";
 
 // Varibles to count the guesses and wins
 let strikes = 8;
@@ -25,9 +26,16 @@ let wins = 0;
 
 // Taking the length of the secret word, and pushing placeholder _'s it into the secret array
 for (i = 0; i < word.length; i++) {
-    secret.push("_ ");
+    console.log(word[i]);
+    if (word[i] === " ") {
+        secret.push(" - ");
+        console.log(secret);
+    } else {
+
+        secret.push("_ ");
+    }
 }
-console.log(secret);
+// console.log(secret);
 
 //Flag Variable to give the game a "state", for recursion.
 let isGameOver = false;
@@ -61,7 +69,7 @@ document.onkeyup = function initialize() {
     instructions_span.innerHTML = ('<input id="resetButton" type="button" value="PLEASE TYPE A LETTER!" onclick="reset();" />');
     availLetters_span.innerHTML = available.join(" ");
     pickedLtrs_span.innerHTML = pickedLtrs.join(" ");
-    despacer();
+    // despacer();
     play();
     rain_audio.play();
 };
@@ -69,6 +77,7 @@ document.onkeyup = function initialize() {
 // Reset function to be called later after game ends
 
 function reset() {
+    isGameOver = false;
     word = puzzles[Math.floor(Math.random() * puzzles.length)];
     console.log(word);
     strikes = 8;
@@ -87,9 +96,14 @@ function reset() {
     instructions_span.innerHTML = ('<input id="resetButton" type="button" value="PLEASE TYPE A LETTER!" onclick="reset();" />');
     availLetters_span.innerHTML = available.join(" ");
     pickedLtrs_span.innerHTML = pickedLtrs.join(" ");
-    despacer();
+    // despacer();
     play();
     rain_audio.play();
+}
+//If Functions for only allowing letters from "Available" Array  
+
+function checkPicked(userGuess, pickedLtrs) {
+    return pickedLtrs.indexOf(userGuess) === -1;
 }
 
 //Main Play Sequence
@@ -99,7 +113,9 @@ function play() {
     //keyup listener to get letter and convert to uppercase for user error
     document.onkeyup = function () {
         let userGuess = String.fromCharCode(event.keyCode).toUpperCase();
-        console.log(userGuess);
+
+        // wordDisplay = wordDisplay.toUpperCase() + e.key.toUpperCase();
+
 
         //removes the key pressed from the available array
         var i = available.indexOf(userGuess);
@@ -112,30 +128,22 @@ function play() {
             pickedLtrs_span.innerHTML = pickedLtrs.join(" ");
         }
 
-        //If Functions for only allowing letters from "Available" Array  
-
-        if (!(userGuess.includes(userGuess))) {
-            pickedLtrs.push(userGuess);
-        }
-
 
         //if the Guessed letter isn't an index of word, remove 1 from the strike pool. If hits Zero, you lose
 
-        if (word.indexOf(userGuess) < 0) {
+        if (word.indexOf(userGuess) < 0 && isGameOver == false) {
             strikes--;
+            console.log(pickedLtrs.indexOf(userGuess), pickedLtrs, userGuess)
+            // if (!available.indexOf(userGuess) == -1){    
+            // strikes--;
+            // }
             lives_span.innerHTML = strikes;
             if (strikes == 0) {
                 // instructions_span.innerHTML = "YOU LOSE! PLAY AGAIN?";
                 instructions_span.innerHTML = ('<input id="resetButton" type="button" value="YOU LOSE! CLICK HERE TO PLAY AGAIN!" onclick="reset();" />');
                 puzzle_div.innerHTML = word;
                 laugh_audio.play();
-                //Rework this into a on screen button
-                // let playAgain = confirm("YOU LOSE. The Answer was " + word + "\n Do you want to play again?");
-                // if (playAgain == true) {
-                //     reset();
-                // } else {
-                //     alert("No problem. Have a nice day!");
-                //     toggler();
+                isGameOver = true;
             }
         }
 
@@ -145,22 +153,16 @@ function play() {
                 // Each time the guess letter is found, add it as a good guess in the same spot
                 if (word[i] === userGuess) {
                     secret[i] = userGuess;
-                    console.log(secret);
+                    // console.log(secret);
                     puzzle_div.innerHTML = secret.join("");
 
-
-                    if (secret.indexOf("_ ") == -1) {
+                    if (secret.indexOf("_ ") == -1 && isGameOver == false) {
                         wins++;
                         wins_p.innerHTML = wins;
                         instructions_span.innerHTML = ('<input id="resetButton" type="button" value="YOU GOT IT! CLICK HERE TO PLAY AGAIN!" onclick="reset();" />');
                         haunting_audio.play();
-                        //Rework this into a screen button
-                        // let playAgain = confirm("YOU WIN. The Answer was " + word + "\n Do you want to play again?");
-                        // if (playAgain == true) {
-                        //     reset();
-                        // } else {
-                        //     alert("No problem. Have a nice day!");
-                        //     toggler();
+                        isGameOver = true;
+
                     }
                 }
 
@@ -169,12 +171,6 @@ function play() {
         }
     }
 }
-
-
-
-console.log(isGameOver);
-//Joe's Thought - the game is over "flag". Let Game Over = False. Game over flips to True on end.
-//run a function in the code if statement. Restarta dn set flag back 
 
 
 // // Other keys do nothing. Repeat key pushes of the same letter do nothing (don't count as strikes)
